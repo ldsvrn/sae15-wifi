dd8233b2cd38c95b4b062f2cb3bb0f9d60d3fa10
frais64 <louis2555@orange.fr>
2022-01-24 23:10:58 +0100

    small fixes

--- src/data/clean.py
+++ src/data/clean.py
@@ -7,8 +7,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Cleans results')
     parser.add_argument("-i", help="path of the input file", required=True)
     parser.add_argument("-o", help="path of the output file", required=True)
-    parser.add_argument("--networks", help="comma separated lists of networks")
-    parser.add_argument("--keepids", help="keep IDs that are not integers", nargs='?', const=True, type=bool)
+    parser.add_argument("--networks", help="comma separated list of networks")
+    parser.add_argument("--keepids", help="keep IDs that are not integers", nargs='?', const=True, type=bool ,default=False)
     args = parser.parse_args()
 
     df = pd.read_csv(args.i)
@@ -17,15 +17,18 @@ if __name__ == '__main__':
     # if null value, delete the row
     df.dropna(inplace=True)
 
-    if args.networks != None:
+    # delete all duplicates
+    df.drop_duplicates(inplace=True)
+
+    if args.networks is not None:
         networks = args.networks.split(",")
         for x in df.index:
             if df.loc[x, "SSID"] not in networks:
                 df.drop(x, inplace=True)
 
-    # TODO: quel résaux wifi garder? ExpId avec virgules???
+    # TODO: quel réseaux wifi garder? ExpId avec virgules???
 
-    if args.keepids == False:
+    if not args.keepids:
         # pour l'instant, on ne garde pas les ExpId à virgules
         for x in df.index:
             if not df.loc[x, "ExpId"].is_integer():
